0f0eb1be779bf66e34e5d2d2e6ea0236f6dfff07
Yves ANDOLFATTO <contact@link-webcreations.fr>
2013-06-10 21:03:50 +0200

    maj to-top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,26 @@
     Author:
     Yves ANDOLFATTO
   =========================================================================== */
-  
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+!function ($) {
+  $(function(){
+
+    // back to top
+    setTimeout(function () {
+      $('.top').affix({
+        offset: {
+          top: function () { return $window.width() <= 980 ? 290 : 210 }
+        , bottom: 270
+        }
+      })
+    }, 100)
+
+    setTimeout(function () {
+      $('.to-top').affix()
+    }, 100)
+
+  })
+}(window.jQuery)
